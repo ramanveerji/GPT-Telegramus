a3f7ee134493e528381cc61d33b786cfc3a54f1c
F33RNI <xxoinvizionxx@gmail.com>
2024-04-19 15:03:44 -0500

    build(version): bump version to 5.4.0

--- _version.py
+++ _version.py
@@ -20,7 +20,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from packaging import version
 
-__version__ = "5.3.0"
+__version__ = "5.4.0"
 
 
 def version_major() -> int:
